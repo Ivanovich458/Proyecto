c307129ccd57362234b6f9619507041e2ab8c3e7
Ivanovich458 <109997774+Ivanovich458@users.noreply.github.com>
2022-09-06 16:12:05 -0300

    Update UsuarioAPI.cs

--- API/Model/UsuarioAPI.cs
+++ API/Model/UsuarioAPI.cs
@@ -1,10 +1,38 @@
 ﻿
-    public class Usuario
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Contraseña { get; set; }
+    public string NombreUsuario { get; set; }
+    public double Mail { get; set; }
+
+
+    public Usuario()
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Contraseña { get; set; }
-        public string NombreUsuario { get; set; }
-        public string Mail { get; set; }
+        this.Id = 0;
+        this.Nombre = String.Empty;
+        this.Apellido = String.Empty;
+        this.NombreUsuario = String.Empty;
+        this.Contraseña = String.Empty;
+        this.Mail = 0;
     }
+
+    public Usuario(int id,
+                   string Nombre,
+                   string Apellido,
+                   string NombreUsuario,
+                   string Contraseña,
+                   double Mail)
+    {
+        this.Id = id;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.NombreUsuario = NombreUsuario;
+        this.Contraseña = Contraseña;
+        this.Mail = Mail;
+    }
+
+
+}
