1da0050d6f4161798346bdc64d0adc23311db5dd
Ivanovich458 <109997774+Ivanovich458@users.noreply.github.com>
2022-09-06 16:13:50 -0300

    Update VentaAPI.cs

--- API/Model/VentaAPI.cs
+++ API/Model/VentaAPI.cs
@@ -1,9 +1,16 @@
-﻿namespace MiPrimeraApi
+namespace MiPrimeraApi
 {
     public class Venta
     {
         public int Id { get; set; }
         public string Comentarios { get; set; }
 
+        public Venta(int Id, string Comentarios)
+        {
+            this.Id = 0;
+            this.Comentarios = string.Empty;
+        }
+        
+        
     }
 }
