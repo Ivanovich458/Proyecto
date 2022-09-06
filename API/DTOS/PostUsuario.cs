89fa15227976ab7c05826d22f19c33f5c0d4d8fc
Ivanovich458 <109997774+Ivanovich458@users.noreply.github.com>
2022-09-06 16:11:26 -0300

    Update PostUsuario.cs

--- API/DTOS/PostUsuario.cs
+++ API/DTOS/PostUsuario.cs
@@ -2,6 +2,7 @@
 {
     public class PostUsuario
     {
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Contraseña { get; set; }
