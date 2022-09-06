d4b54f04c5c117843b64e96b1795618071834b12
Ivanovich458 <109997774+Ivanovich458@users.noreply.github.com>
2022-09-06 16:13:24 -0300

    Update ProductoAPI.cs

--- API/Model/ProductoAPI.cs
+++ API/Model/ProductoAPI.cs
@@ -3,10 +3,24 @@
     public class Producto
     {
         public int Id { get; set; }
-        public string Descripciones { get; set; }
+        public string? Descripciones { get; set; }
         public double Costo { get; set; }
         public double PrecioVenta { get; set; }
         public int Stock { get; set; }
         public int IdUsuario { get; set; }
+
+        public Producto(int Id, string? Descripciones, double Costo, double PrecioVenta, int Stock, int IdUsuario)
+        {
+            this.Id = 0;
+            this.Descripciones = string.Empty;
+            this.Costo = 0;
+            this.PrecioVenta = 0;
+            this.Stock = 0;
+            this.IdUsuario = 0;
+        }
+
     }
+
+
+
 }
